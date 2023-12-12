33bee0653bc4f308e0a40519f11a1e280afdb725
Hussein <elmasrih78@gmail.com>
2023-12-12 17:22:12 +0200

    initial commit

---
+++ Controllers/MarketsController.cs
@@ -0,0 +1,29 @@
+﻿using RagsAndTags.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RagsAndTags.Controllers
+{
+    public class MarketsController : Controller
+    {
+        static List<Market> markets = new List<Market>
+        {
+            new Market {Id = 1, Name = "Sppinneys", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"},
+            new Market {Id = 1, Name = "Carffour", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"},
+            new Market {Id = 1, Name = "Makhazen", Open = "9:00-AM", Close = "12:00-AM", Image = "images/super1.png"}
+        };
+        public IActionResult Index()
+        {
+            return View(markets);
+        }
+
+        public IActionResult ViewItems()
+        {
+            return View();
+        }
+
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
